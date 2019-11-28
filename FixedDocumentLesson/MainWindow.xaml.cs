9296a5e417bfdf7044413c004067f75fdd1166c8
e.alihangt@gmail.com <e.alihangt@gmail.com>
2019-11-28 21:22:10 +0600

    Add project files.

---
+++ FixedDocumentLesson/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Xps.Packaging;
+using System.IO;
+using System.Windows.Xps;
+
+namespace FixedDocumentLesson
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UploadXpxFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var isProcessed = openFileDialog.ShowDialog();
+            if (isProcessed.Value)
+            {
+                var xpsFileName = openFileDialog.FileName;
+                using (var xpsDocument = new XpsDocument(xpsFileName, FileAccess.ReadWrite)
+                {
+                    documentViewer.Document = xpsDocument.GetFixedDocumentSequence();
+                }
+            }
+        }
+        private void SaveXps(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            var hasres = saveFileDialog.ShowDialog();
+            if (hasres.Value)
+            {
+            var xpsFileName = openFileDialog.FileName;
+            using (var xpsDocument = new XpsDocument(xpsFileName, FileAccess.ReadWrite)
+            {
+                XpsDocumentWriter writer = XpsDocument.CreateXpsDocumentWriter(xpsDocument);
+                writer.Write()
+            }
+    }
+        }
+    }
+
+}
